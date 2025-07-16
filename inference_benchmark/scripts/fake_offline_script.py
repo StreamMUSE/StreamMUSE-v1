55ea328dc895e38dd104745c160858ac0abb085a
S-tanley <2962786177@qq.com>
2025-07-16 21:45:53 +0400

    new script for inference benchmark

---
+++ inference_benchmark/scripts/fake_offline_script.py
@@ -0,0 +1,55 @@
+"""
+Template Benchmark Script
+"""
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+from fake_offline import fake_offline_sampling
+
+class fake_offline_script:
+    def __init__(self):
+        self.name = self.__class__.__name__
+
+    def run(self,
+            model_path,
+            input_midi,
+            output_midi,
+            prompt_len=200,
+            n_rounds=200,
+            temperature=1.0,
+            gen_interval_ticks=1,
+            gen_seq_len=2,
+            latency=0
+        ):
+
+        output_dir = os.path.dirname(output_midi)
+        if output_dir and not os.path.exists(output_dir):
+            os.makedirs(output_dir, exist_ok=True)
+
+        print(f'Read input MIDI from {input_midi}')
+        fake_offline_sampling(
+            model_path=model_path,
+            init_midi_path=input_midi,
+            n_rounds=n_rounds,
+            id_num=0,
+            prompt_len=prompt_len,
+            temperature=temperature,
+            gen_interval_ticks=gen_interval_ticks,
+            gen_seq_len=gen_seq_len,
+            latency=latency,
+            save_path=output_midi
+        )
+
+        print(f'Save output MIDI to {output_midi}')
+
+if __name__ == "__main__":
+    # Example usage
+    script = fake_offline_script()
+    script.run(
+        model_path="results/ModelBaseline/cp_transformer_909+ac+1k7_trackemb_interleavepos_v0.2_large_batch_40_schedule.epoch=00.val_loss=0.90296.ckpt",
+        input_midi="/home/bowen.zheng/Documents/StreamMUSE/input/acc/001.mid",
+        output_midi="/home/bowen.zheng/Documents/StreamMUSE/output/001_output.mid",
+        prompt_len=200,
+        gen_seq_len=2, # 2 frames means 1 logical tick
+        temperature=1.0
+    )
