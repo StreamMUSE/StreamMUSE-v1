6a5121110a46e4431bccac78591027c97bd5eec7
S-tanley <2962786177@qq.com>
2026-01-28 15:34:27 -0600

    small change about the upload file

--- upload_file.py
+++ upload_file.py
@@ -2,8 +2,8 @@ from huggingface_hub import HfApi
 
 # --- 配置你的上传信息 ---
 repo_id = "S-tanley/M2A"
-local_folder_path = "./results/ModelAiraUnique0.5" # 要上传的本地文件夹
-path_in_repo = "./ModelAiraUnique0.5" # 在远程仓库中保存的路径
+local_folder_path = "/Users/zhengbowen/Library/Containers/com.tencent.xinWeChat/Data/Library/Application Support/com.tencent.xinWeChat/2.0b4.0.9/e2819ded95e6751ca55d773ffdf7a515/Message/MessageTemp/5bca8bd6e5be5cf9194667dfd69c5add/File/RT_Accompaniment/checkpoints" # 要上传的本地文件夹
+path_in_repo = "./ModelLekai" # 在远程仓库中保存的路径
 
 # --- 上传逻辑 ---
 api = HfApi()
